f755db40b58ca4a1bb22f066aded53846c895931
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-04-30 21:05:43 +0100

    XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("XCARET").keyx()
+       __import__("XCARET").login()
    except Exception as a:
        exit(str(a))
