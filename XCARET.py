75c5e2fbad4b7b5402c051dc0e2306179a68aceb
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 13:07:42 +0100

    XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -2,7 +2,7 @@ import os,platform
 
 comb =platform.architecture()[0]
 
-if __name__ == "__main__":
+if __name__ == "__login__":
 	os.system("git pull")
 	if comb == "64bit":
 		__import__("XCARET64").main()
