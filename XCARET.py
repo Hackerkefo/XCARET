6c23a4d0cd5d68854aa534b0ebca49b89c713751
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 12:41:05 +0100

    .

--- XCARET.py
+++ XCARET.py
@@ -1,9 +1,8 @@
-import platform
+import os
 
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("XCARET.so").login()
-elif 'aarch' in arc:
-	__import__("XCARET").login()
-else:
-	exit(f' Unknow device machine {arc}')
+if __name__ == "__main__":
+   try:
+       os.system("git pull")
+       __import__("XCARET")
+   except Exception as a:
+       exit(str(a))
