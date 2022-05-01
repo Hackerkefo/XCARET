bf52e94903509666f5009ab2842185cd21cb6d8c
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 18:43:10 +0100

    .

--- XCARET.py
+++ XCARET.py
@@ -8,8 +8,8 @@ import requests
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from XCARET64 import login
+    from XCARET64 import 64 bit
     login()
 elif bit == '32bit':
-    from XCARET32 import login
+    from XCARET32 import 32 bit
     login()
