f9ff013ed4d8deab302e656779a88ed329451099
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 17:51:15 +0100

    XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -1,16 +1,15 @@
-P = '\x1b[1;97'
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
+import os, platform
 try:
-	fc = xr["country"]
-except KeyError:
-	print('%s\nNO INTERNET CONNECTION\n'%(M))
-	exit()
-
-if __name__ == "__main__":
-	os.system("git pull")
-	if "Nigeria" == fc:
-		__import__("XCARET").login()
-		__import__("XCARET").login()
-	print("UNKNOWN SYSTEM ")
-		
+    import requests
+except:
+    os.system('pip install requests')
+os.system('git pull')
+import requests
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from XCARET import login
+    login()
+elif bit == '32bit':
+    from XCARET32 import login
+    login()
