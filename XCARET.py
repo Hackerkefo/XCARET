ad7dde359a66ca386453b9675e60ea414ed7e0d8
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 18:24:50 +0100

    Update XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -1,24 +1,14 @@
-#Chigozieworldwide_coding
-import os, time, platform
-os.system("cd $HOME/")
+import os, platform
 try:
-    import lolcat
-except ImportError:
-    os.system("pip2 install lolcat")
-try:
-    import requests
-except ImportError:
-    os.system("pip2 install requests")
-    os.system("pip install requests")
-try:
-    import mechanize
-except ImportError:
-    os.system("pip2 install mechanize")
-    os.system("pip install mechanize")
-rana=platform.architecture()[0]
-if comb=="32bit":
-    import XCARET32
-    XCARET32.login()
-elif comb=="64bit":
-    import XCARET64
-    XCARET64.login()
+   import requests
+except:
+   os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from XCARET64 import login
+    login()
+elif bit == '32bit':
+    from XCARET32 import login
+    login()
