84718d19f329825ac6720b2aa874caacf8e0bc8e
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 12:53:18 +0100

    .

--- XCARET.py
+++ XCARET.py
@@ -5,9 +5,9 @@ comb =platform.architecture()[0]
 if name == "main":
  os.system("git pull")
  if comb == "64bit":
-  import("xctate64").main()
+  import("XCARET64").login()
  elif comb == "32bit":
-  import("xctate32").main()
+  import("XCARET32").login()
  else:
   print("UNKNOWN SYSTEM ")
   exit()
