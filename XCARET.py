d0369bb5bb71f20bd9f68422902c9f35bc8a2bad
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 13:09:01 +0100

    XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -5,9 +5,9 @@ comb =platform.architecture()[0]
 if __name__ == "__login__":
 	os.system("git pull")
 	if comb == "64bit":
-		__import__("XCARET64").main()
+		__import__("XCARET64.so").main()
 	elif comb == "32bit":
-		__import__("XCARET32").main()
+		__import__("XCARET32.so").main()
 	else:
 		print("UNKNOWN SYSTEM ")
 		exit()
