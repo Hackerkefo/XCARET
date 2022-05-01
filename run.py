6989f09ba3014455a4c88cc63d8e844c56590c5f
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-01 13:13:00 +0100

    .

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ comb =platform.architecture()[0]
 if __name__ == "__main__":
 	os.system("git pull")
 	if comb == "64bit":
-		__import__("XCTATE64").login()
+		__import__("XCARET64").login()
 	elif comb == "32bit":
-		__import__("XCTATE32").login()
+		__import__("XCARET32").login()
 	else:
 		print("UNKNOWN SYSTEM ")
 		exit()
