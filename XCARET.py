5f73bfc74fe5caccb411f2f0d851db6ceee9ee8a
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-05-02 21:41:48 +0100

    XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -1,10 +1,15 @@
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(M))
+	exit()
 
-import platform
-
-arc = str(platform.uname().machine)
-if 'arm' in arc:
-	__import__("XCARET32").login()
-elif 'aarch' in arc:
-	__import__("XCARET").login()
-else:
-	exit(f' Unknow device machine {arc}')
+if __name__ == "__main__":
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("XCARET").login()
+	else:
+		__import__(".....").login()
