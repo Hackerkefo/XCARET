89cc63133353980e38e39bb73a583e535cfa4037
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-17 17:25:45 +0100

    Update XCARET.py

--- XCARET.py
+++ XCARET.py
@@ -1,16 +1,20 @@
-termux-setup-storage
-P = '\x1b[1;97m'
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
-try:
-	fc = xr["country"]
-except KeyError:
-	print('%s\nBAD INTERNET CONNECTION\n'%(P))
-	exit()
-
+import os
+import platform
+import webbrowser
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('mkdir /sdcard/CHI-DATA')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/OK')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/CP')
+except:pass
+try:os.system('mkdir /sdcard/CHI-DATA/TAP-A2F')
+except:pass
+try:os.system('touch .prox.txt')
+except:pass
 if __name__ == "__main__":
-	os.system("git pull")
-	if "Nigeria" == fc:
-		__import__("XCARET").login()
-	else:
-		__import__("XCARET").login()
+        try:
+                __import__("XCARET").login()
+        except Exception as e:
+                exit(str(e))
