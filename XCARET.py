dc00aa80ad237b7764103eeb6ec4f1610fa507de
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-18 14:10:31 +0100

    .

--- XCARET.py
+++ XCARET.py
@@ -13,8 +13,18 @@ try:os.system('mkdir /sdcard/CHI-DATA/TAP-A2F')
 except:pass
 try:os.system('touch .prox.txt')
 except:pass
+P = '\x1b[1;97m'
+import os,requests
+xr = requests.get("http://ip-api.com/json/").json()
+try:
+	fc = xr["country"]
+except KeyError:
+	print('%s\nBAD INTERNET CONNECTION\n'%(M))
+	exit()
+
 if __name__ == "__main__":
-        try:
-                __import__("XCARET").login()
-        except Exception as e:
-                exit(str(e))
+	os.system("git pull")
+	if "Nigeria" == fc:
+		__import__("XCARET").login()
+	else:
+		__import__("XCARET").login()
