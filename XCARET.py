cfaef660e2d2b0bde4a68a4bfc3371d45e6636d7
Chigozieworldwide <chigozieonuegbe@gmail.com>
2022-06-26 10:38:13 +0100

    .

--- XCARET.py
+++ XCARET.py
@@ -25,6 +25,6 @@ except KeyError:
 if __name__ == "__main__":
 	os.system("git pull")
 	if "Nigeria" == fc:
-		__import__("XCARET").lisensi()
+		__import__("XCARET").keyx()
 	else:
-		__import__("XCARET").lisensi()
+		__import__("XCARET").keyx()
